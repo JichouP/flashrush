6328edd58ee730dcafef15f13bf109484c3223c4
JichouP <akkunn100@gmail.com>
2019-01-31 13:19:41 +0900

    ディレクトリ構造変更
    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import Top from './Top';
-import DrawableCanvas from './DrawableCanvas';
-import AutoCorrect from './AutoCorrect';
+import Top from './components/Top';
+import AutoCorrect from './components/AutoCorrect';
 
 export default class App extends Component {
   render() {
