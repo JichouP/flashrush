0d1940598e22b6e24f423e1099dab2432c4be12a
JichouP <akkunn100@gmail.com>
2019-02-01 00:07:47 +0900

    AppBar, Drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Drawer from '@material-ui/core/Drawer';
 import Top from './components/Top';
 import AutoCorrect from './components/AutoCorrect';
 import useFonts from './util/useFonts';
+import ResponsiveDrawer from './components/ResponsiveDrawer';
 useFonts();
 export default class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Top} />
-          <Route path="/canvas" component={AutoCorrect} />
-        </Switch>
-      </Router>
+      <div>
+        <ResponsiveDrawer />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Top} />
+            <Route path="/canvas" component={AutoCorrect} />
+          </Switch>
+        </Router>
+      </div>
     );
   }
 }
--- src/util/useFonts.ts
+++ src/util/useFonts.ts
@@ -1,6 +1,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBackspace } from '@fortawesome/free-solid-svg-icons';
+import { faBackspace, faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default () => {
-  library.add(faBackspace);
+  library.add(faBackspace, faBars);
 };
